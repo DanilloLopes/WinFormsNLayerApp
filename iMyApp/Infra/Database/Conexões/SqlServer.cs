5e4e1246c90906ca6e719dabbbc61dca3e900f4f
Danillo <lopessousa2004@gmail.com>
2023-09-12 21:58:39 -0300

    Implemnetando repositorio de cargos

--- iMyApp/Infra/Database/Conexões/SqlServer.cs
+++ iMyApp/Infra/Database/Conexões/SqlServer.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,10 @@ namespace Database.Conexoes
 {
     internal class SqlServer
     {
+        internal static string StrConexao()
+        {
+            return @"Data Source=.\SQLEXPRESS;Initial Catalog=iMyApp;User ID=sa;Password=sql2022";
+        }
+        
     }
 }
--- iMyApp/Infra/Database/Repositorios/CargoRepository.cs
+++ iMyApp/Infra/Database/Repositorios/CargoRepository.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using Database.Conexoes;
+using Microsoft.Data.SqlClient;
+using Negocio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Database.Repositorios
 {
-    internal class CargoRepository
+    public class CargoRepository
     {
+        public int Inserir(Cargo cargo)
+        {
+            var stringConexao = SqlServer.StrConexao();
+
+            var sqlConnection = new SqlConnection(stringConexao);
+
+            sqlConnection.Open();
+
+            var sql = @"INSERT INTO [dbo].[Cargo]
+           ([Nome]
+           ,[Status]
+           ,[CriadoEm]
+           ,[CriadoPor]
+           ,[AlteradoEm]
+           ,[AlteradoPor])
+     VALUES
+           (@nome,
+            @status,
+            @criadoEm,
+            @criadoPor,
+            @alteradoEm,
+            @alteradoPor)";
+
+            var cmd = new SqlCommand();
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@nome", cargo.Nome);
+            cmd.Parameters.AddWithValue("@status", cargo.Status);
+            cmd.Parameters.AddWithValue("@criadoEm", cargo.CriadoEm);
+            cmd.Parameters.AddWithValue("@criadoPor", cargo.CriadoPor);
+            cmd.Parameters.AddWithValue("@alteradoEm", cargo.AlteradoEm);
+            cmd.Parameters.AddWithValue("@alteradoPor", cargo.AlteradoPor);
+
+            sqlConnection.Close();
+        }
     }
 }
